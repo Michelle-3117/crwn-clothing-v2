cf1497bacdf8f0cf32d87dee42fc9872417e7f5d
Michelle Uzonna <mitchelleuzonna@gmail.com>
2022-10-24 23:22:45 +0100

    Created category and directory component folders and populated it

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
+import './categories.styles.scss'
+import Directory from './components/directory/Directory';
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl:
+        "https://images.pexels.com/photos/10074978/pexels-photo-10074978.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl:
+        "https://images.pexels.com/photos/1124468/pexels-photo-1124468.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl:
+        "https://images.pexels.com/photos/1478442/pexels-photo-1478442.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 4,
+      title: "Women",
+      imageUrl:
+        "https://images.pexels.com/photos/2657594/pexels-photo-2657594.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      id: 5,
+      title: "Men",
+      imageUrl:
+        "https://images.pexels.com/photos/3568518/pexels-photo-3568518.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ];
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+   <Directory categories={categories}/>
   );
 };
 
---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Category from '../category-item/Category';
+import './directory-styles.scss'
+
+const Directory = ({categories}) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <Category key={category.id} category={category} />
+      ))}
+    </div>
+  );
+}
+
+export default Directory
